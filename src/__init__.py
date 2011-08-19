a5e2f2d5ff19ab7d47dc0ab2fc955c9512fe6b3e
Dirk Meyer <dischi@freevo.org>
2011-08-19 20:33:31 +0000

    improve import
    git-svn-id: svn://svn.freevo.org/kaa/trunk/WIP/candy2@4502 a8f5125c-1e01-0410-8897-facf34644b8e

--- src/__init__.py
+++ src/__init__.py
@@ -3,8 +3,8 @@ import os
 
 if not sys.argv[0].startswith(os.path.dirname(__file__)):
 
-    from core import Context, Color, Font
-    from widgets import Widget, Group, Rectangle, Text
+    from core import Context, Color, Font, is_template
+    from widgets import *
     from stage import Stage
 
     import candyxml
