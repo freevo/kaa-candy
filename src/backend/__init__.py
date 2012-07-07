b44989c74ffe99ee0acde0217110338553acac67
Dirk Meyer <dischi@freevo.org>
2012-07-07 20:18:57 +0200

    Licence change of the candy backend to GPL
    The kaa.candy server switches to GLP instead of GPL. Just like
    kaa.beacon there is a call to kaa.metadata which is GPL.

--- src/backend/__init__.py
+++ src/backend/__init__.py
@@ -1,3 +1,28 @@
+# -*- coding: iso-8859-1 -*-
+# -----------------------------------------------------------------------------
+# kaa-candy - Fourth generation Canvas System using Clutter as backend
+# Copyright (C) 2011-2012 Dirk Meyer
+#
+# Based on various previous attempts to create a canvas system for
+# Freevo by Dirk Meyer and Jason Tackaberry.  Please see the file
+# AUTHORS for a complete list of authors.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MER-
+# CHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+# -----------------------------------------------------------------------------
+
 import sys
 if 'kaa.candy' in sys.modules:
     raise RuntimeError('importing the backend is forbidden for the client')
--- src/backend/widgets/mplayer.py
+++ src/backend/widgets/mplayer.py
@@ -17,26 +17,23 @@
 # kaa-candy - Fourth generation Canvas System using Clutter as backend
 # Copyright (C) 2012 Dirk Meyer
 #
-# First Version: Dirk Meyer <dischi@freevo.org>
-# Maintainer:    Dirk Meyer <dischi@freevo.org>
-#
 # Based on various previous attempts to create a canvas system for
 # Freevo by Dirk Meyer and Jason Tackaberry.  Please see the file
 # AUTHORS for a complete list of authors.
 #
-# This library is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License version
-# 2.1 as published by the Free Software Foundation.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-# This library is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MER-
+# CHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
 #
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-# 02110-1301 USA
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #
 # -----------------------------------------------------------------------------
 
